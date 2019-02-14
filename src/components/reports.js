deaacf69451acb322a25289092e940365959fae1
Rahul Sai <noneuser2183@gmail.com>
2019-02-14 20:50:36 +0530

    report view changes

--- src/components/reports.js
+++ src/components/reports.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, View} from 'react-native';
 import Footerapp from './footerapp';
-import { Container, Header, Card, CardItem , Content, Button, Text, Body} from 'native-base';
+import { Container, Header, Card, CardItem , Content, Button, Text, Body ,H3,Right,List,ListItem} from 'native-base';
+import { Actions } from 'react-native-router-flux';
 
 export default class reports extends React.Component {
   render() {
@@ -10,21 +11,21 @@ export default class reports extends React.Component {
       <Container>
         <Header><Text style={{color:'white',marginTop:15,fontSize:20 }}>Reports</Text></Header>
         <Content padder>
-          <Card>
-            <CardItem header bordered>
-              <Text>NativeBase</Text>
-            </CardItem>
-            <CardItem bordered>
+        <List>
+            <ListItem thumbnail>
               <Body>
-                <Text>
-                  NativeBase is a free and open source framework that enable
-                </Text>
+                <H3 style={{marginBottom:5}}>Oncology Report</H3>
+                <Text style={{marginBottom:5}}>Dr. Rajmohan</Text>
+                <Text note>From the prognosis of the report, there is no swelling in the interior cruciate part of 3 B</Text>
               </Body>
-            </CardItem>
-            <CardItem footer bordered>
-              <Text>Geeky Ants</Text>
-            </CardItem>
-          </Card>
+              <Right>
+                <Text note style={styles.centerText}>03/06/2018</Text>
+                <Button transparent onPress={Actions.pdf.bind(this)}>
+                  <Text>View Report</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
         </Content>
       </Container>
         <Footerapp/>
@@ -32,3 +33,23 @@ export default class reports extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 5,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
