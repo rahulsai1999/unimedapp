9d6d3c2459900633515f7f754ed08fee209f8883
Rahul Sai <noneuser2183@gmail.com>
2019-02-17 00:54:52 +0530

    form made

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, View} from 'react-native';
-import { Container, Header, Card, CardItem , Content, Button, Text, Body} from 'native-base';
+import { Container, Header, Form, Label, Input,Item, Card, CardItem , Content, Button, Text, Body} from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
 export default class splashafter extends React.Component {
@@ -10,13 +10,29 @@ export default class splashafter extends React.Component {
       <Container>
         <Header><Text style={{color:'white',marginTop:15,fontSize:20 }}>Login</Text></Header>
         <Content padder>
-          <Text>Login</Text>
-          <Button onPress={Actions.home.bind(this)}><Text>Login</Text></Button>
-          <Text>Don't have an account</Text>
-          <Button onPress={Actions.register.bind(this)}><Text>Sign Up</Text></Button>
+            <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+          <Button block success style={{marginTop:20,borderRadius:30,alignSelf:'center'}} onPress={Actions.home.bind(this)}><Text>Login</Text></Button>
+          <Text>Don't have an account?</Text>
+          <Button block warning style={{marginTop:20,borderRadius:30,alignSelf:'center'}} onPress={Actions.register.bind(this)}><Text>Sign Up</Text></Button>
         </Content>
       </Container>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  centerbutt:{
+    alignItems: 'center',
+    justifyContent:'center'
+  }
+});
