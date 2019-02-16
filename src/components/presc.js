5a89a598f54086abc08c5320176537cfdbcf512a
Rahul Sai <noneuser2183@gmail.com>
2019-02-16 15:04:28 +0530

    prescriptions chenged further required

--- src/components/presc.js
+++ src/components/presc.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, View} from 'react-native';
 import Footerapp from './footerapp';
-import { Container, Header, Card, CardItem , Content, Button, Text, Body,ListItem,CheckBox} from 'native-base';
+import { Container, Header, Card, CardItem , Content, Button, Text, Body,ListItem,CheckBox,Right} from 'native-base';
+import Icon from 'react-native-vector-icons/Feather';
+import Icon2 from 'react-native-vector-icons/Ionicons'
 
 export default class presc extends React.Component {
   render() {
@@ -9,26 +11,39 @@ export default class presc extends React.Component {
       <View style={{height:1460}}>
       <Container>
         <Header><Text style={{color:'white',marginTop:15,fontSize:20 }}>Prescriptions</Text></Header>
-        <Content padder>
-        <ListItem>
-            <CheckBox/>
-            <Body>
-              <Text>Paracetamol</Text>
-            </Body>
-          </ListItem>
-          <ListItem>
-            <CheckBox/>
-            <Body>
-              <Text>Betadine</Text>
-            </Body>
-          </ListItem>
-          <ListItem>
-            <CheckBox/>
-            <Body>
-              <Text>Amoxicillin</Text>
-            </Body>
-          </ListItem>
-          
+        <Content>
+        <Card>
+          <CardItem header>
+              <Text>Dr.Rajmohan</Text>
+              <Right>
+                <Text>29/03/2019</Text>
+              </Right>
+          </CardItem>
+          <CardItem>
+            <Text>Paracetamol</Text>
+              <Right>
+                <Icon name="sunrise" size={25} color="orange" /><Icon name="sun" size={25} color="orange" />
+                <Icon name="sunset" size={25} color="orange" />
+                <Icon2 name="ios-cloudy-night" size={25} color="black" />
+              </Right>
+          </CardItem>
+          <CardItem>
+            <Text>Paracetamol</Text>
+              <Right>
+                <Icon name="sunrise" size={25} color="orange" /><Icon name="sun" size={25} color="orange" />
+                <Icon name="sunset" size={25} color="orange" />
+                <Icon2 name="ios-cloudy-night" size={25} color="black" />
+              </Right>
+          </CardItem>
+          <CardItem>
+            <Text>Paracetamol</Text>
+              <Right>
+                <Icon name="sunrise" size={25} color="orange" /><Icon name="sun" size={25} color="orange" />
+                <Icon name="sunset" size={25} color="orange" />
+                <Icon2 name="ios-cloudy-night" size={25} color="black" />
+              </Right>
+          </CardItem>
+        </Card>
         </Content>
       </Container>
         <Footerapp/>
@@ -36,3 +51,4 @@ export default class presc extends React.Component {
     );
   }
 }
+
